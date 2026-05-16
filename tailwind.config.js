176f31e55bf82b82eca2e233a28963620ad453c2
chalatz <chralatz@gmail.com>
2026-05-16 19:28:24 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './_includes/**/*.{js,njk,md}',
+    './_layouts/**/*.{js,njk,md}',
+    './index.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
